574e10e9568a79e02d186b6fa2c495709545cecb
Nick Doiron <ndoiron@mapmeld.com>
2012-08-10 13:57:47 -0700

    add popup with id

--- public/scripts/neighbordiff.js
+++ public/scripts/neighbordiff.js
@@ -1,10 +1,13 @@
+var map, building_pop;
 function init(){
-  var map = new L.Map('map');
+  map = new L.Map('map');
   var toner = 'http://{s}.tile.stamen.com/terrain-lines/{z}/{x}/{y}.png';
   var tonerAttrib = 'Map data &copy; 2012 OpenStreetMap contributors, Tiles &copy; 2012 Stamen Design';
   var tonerLayer = new L.TileLayer(toner, {maxZoom: 18, attribution: tonerAttrib});
   map.addLayer(tonerLayer);
   map.setView(new L.LatLng(32.83895, -83.62913), 15);
+  
+  building_pop = new L.Popup();
   
   var cartodb_leaflet = new L.CartoDBLayer({
     map: map,
@@ -14,10 +17,12 @@ function init(){
     tile_style: "#collegeplusintown{polygon-fill:orange}",
     interactivity: "cartodb_id",
     featureClick: function(ev, latlng, pos, data){
-      console.log(data)
+      console.log(data);
+      building_pop.setLatLng(latlng).setContent("ID: " + data.cartodb_id);
+      map.openPopup(building_pop);
     },
-    featureOver: function(){},
-    featureOut: function(){},
+    //featureOver: function(){},
+    //featureOut: function(){},
     auto_bound: true
   });
   map.addLayer(cartodb_leaflet);
