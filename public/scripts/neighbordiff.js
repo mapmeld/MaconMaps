dd92df941066dca421e0f915f61ed264763b687c
Nick Doiron <ndoiron@mapmeld.com>
2012-08-10 13:52:35 -0700

    change background tiles

--- public/scripts/neighbordiff.js
+++ public/scripts/neighbordiff.js
@@ -1,6 +1,6 @@
 function init(){
   var map = new L.Map('map');
-  var toner = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
+  var toner = 'http://{s}.tile.stamen.com/terrain-lines/{z}/{x}/{y}.png';
   var tonerAttrib = 'Map data &copy; 2012 OpenStreetMap contributors, Tiles &copy; 2012 Stamen Design';
   var tonerLayer = new L.TileLayer(toner, {maxZoom: 18, attribution: tonerAttrib});
   map.addLayer(tonerLayer);
