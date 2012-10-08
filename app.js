6a212bd6904c8c8c9e6f723804a0c3d9af0dd507
Nick Doiron <ndoiron@mapmeld.com>
2012-10-08 10:00:16 -0700

    geospatial index and search of timepoints

--- app.js
+++ app.js
@@ -413,7 +413,7 @@ function replaceAll(src, oldr, newr){
   });
   app.get('/timeline-at', function(req, res){
     // do a query to return complete GeoJSON timeline
-    timepoint.TimePoint.find({ loc: { "$nearSphere": [ 32.837026, lon:  -83.645782 ], $maxDistance: 0.01 } }).limit(10).exec(function(err, timepoints){
+    timepoint.TimePoint.find({ loc: { "$nearSphere": [ 32.837026, -83.645782 ], "$maxDistance": 0.01 } }).limit(10).exec(function(err, timepoints){
       // convert all timepoints into GeoJSON format
       for(var t=0; t<timepoints.length; t++){
         timepoints[t] = {
