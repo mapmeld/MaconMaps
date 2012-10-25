3b42430d11f3de3543af8fdf438b5defa235f05f
Nick Doiron <ndoiron@mapmeld.com>
2012-10-25 19:33:04 -0400

    select map tile style

--- public/scripts/homeswitch.js
+++ public/scripts/homeswitch.js
@@ -1,8 +1,22 @@
 var currentTab = "osmgoogle";
+var currentMap = "mapquest";
 function switchTo(id){
 	document.getElementById(currentTab).style.display = "none";
 	document.getElementById(currentTab + "tab").className = "";
 	document.getElementById(id).style.display = "block";
 	document.getElementById(id + "tab").className = "active";
 	currentTab = id;
+}
+function setTile(id){
+	document.getElementById(currentMap).style.display = "none";
+	document.getElementById(id + "tab").className = "active";
+	var mapurls = document.getElementsByClassName(currentMap + "url");
+	for(var m=0;m<mapurls.length;m++){
+	  mapurls[m].style.display = "none";
+	}
+	currentMap = id;
+	var mapurls = document.getElementsByClassName(currentMap + "url");
+	for(var m=0;m<mapurls.length;m++){
+	  mapurls[m].style.display = "block";
+	}
 }
